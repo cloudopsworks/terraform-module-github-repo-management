10942654eb6db0e39918fc7070cfe4422b693748
Cristian Beraha <cristian@cloudops.works>
2025-06-18 16:33:30 -0300

    Update copyright notices across files and expand documentation
    Aligned copyright years (2021-2025) and updated headers across multiple files. Enhanced README and YAML documentation with detailed usage examples and module configuration instructions for improved user guidance.

--- locals.tf
+++ locals.tf
@@ -1,7 +1,10 @@
 ##
-# (c) 2024 - Cloud Ops Works LLC - https://cloudops.works/
-#            On GitHub: https://github.com/cloudopsworks
-#            Distributed Under Apache v2.0 License
+# (c) 2021-2025
+#     Cloud Ops Works LLC - https://cloudops.works/
+#     Find us on:
+#       GitHub: https://github.com/cloudopsworks
+#       WebSite: https://cloudops.works
+#     Distributed Under Apache v2.0 License
 #
 locals {
   system_name       = format("%s-%s-%s", lower(var.org.organization_unit), lower(var.org.environment_name), lower(var.org.environment_type))
